fa205d0d7023929a7d2037f470e01828ec90a9d2
Bae Haram <unknownfrontenddeveloper@gmail.com>
2021-09-15 14:32:37 +0900

    Compose App component with other subpage/components
    Signed-off-by: Bae Haram <unknownfrontenddeveloper@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
-function App() {
+import { QueryClient, QueryClientProvider } from "react-query";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import ResetPassword from "./pages/ResetPassword";
+
+const queryClient = new QueryClient();
+
+const App = (): React.ReactElement => {
   return (
-    <div className="App">
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <Switch>
+          <Route path="/" component={ResetPassword} />
+        </Switch>
+      </Router>
+    </QueryClientProvider>
   );
-}
+};
 
 export default App;
