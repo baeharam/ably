18530b28ffe32afb1fd4b24bb58e3534747e46c7
Bae Haram <unknownfrontenddeveloper@gmail.com>
2021-09-16 12:34:48 +0900

    Apply redirection for path "/"
    Signed-off-by: Bae Haram <unknownfrontenddeveloper@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 import { QueryClient, QueryClientProvider } from "react-query";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 import ResetPassword from "./pages/ResetPassword";
 import Login from "./pages/Login";
 import User from "./pages/User";
@@ -11,6 +16,9 @@ const App = (): React.ReactElement => {
     <QueryClientProvider client={queryClient}>
       <Router>
         <Switch>
+          <Route exact path="/">
+            <Redirect to="/reset-password" />
+          </Route>
           <Route path="/reset-password" component={ResetPassword} />
           <Route exact path="/login" component={Login} />
           <Route exact path="/user" component={User} />
